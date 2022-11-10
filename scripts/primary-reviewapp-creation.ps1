e2d0c7bc5fb034113cf06c34ccc093f3cefd19df
Julio Garcia <juliocgm83@gmail.com>
2022-11-10 15:55:58 -0500

    try 8

--- scripts/primary-reviewapp-creation.ps1
+++ scripts/primary-reviewapp-creation.ps1
@@ -1,17 +1,28 @@
  param (
-    [Parameter(Mandatory=$false)]
-    [string]$workingDirectoryPath = "lola",
+    [Parameter(Mandatory=$true)]
+    [string]$workingDirectoryPath,
 
     [Parameter(Mandatory=$true)]
     [string]$herokuApiKey,
 
     [Parameter(Mandatory=$true)]
-    [string]$targetHerokuPipelineName
+    [string]$githubAccessToken,
+
+    [Parameter(Mandatory=$true)]
+    [string]$githubRepositoryFullName,
+
+    [Parameter(Mandatory=$true)]
+    [string]$githubFullBranchName,
+
+    [Parameter(Mandatory=$true)]
+    [string]$herokuPipelineName
 )
 
 
 # variables
 $herokuApiBaseURL = "https://api.heroku.com"
+$pullRequestNumber = $githubFullBranchName.Replace("refs/pull/", "").Replace("/merge", "").trim()
+$branchName = $githubFullBranchName.Replace("refs/", "").trim()
 
 # heroku request header definition
 $herokuRequestHeader = @{
@@ -22,7 +33,7 @@ $herokuRequestHeader = @{
 
 # verify if heroku pipeline exists
 $herokuPipelineInstance = $null
-$uri = "$herokuApiBaseURL/pipelines/$targetHerokuPipelineName"
+$uri = "$herokuApiBaseURL/pipelines/$herokuPipelineName"
 try{
     Write-Output "Getting primary HEROKU pipeline details..."
     Write-Output "GET Request URL: $uri"
@@ -42,6 +53,56 @@ catch{
 }
 Write-Output "*************************************************"
 
+# verify if exists already a review app related with the pull request
+$herokuPipelineInstanceID = $herokuPipelineInstance.id
+$uri = "$herokuApiBaseURL/pipelines/$herokuPipelineInstanceID/review-apps"
+Write-Output "Verify if exists already a review app related with the pull request..."
+$appReviewList = Invoke-RestMethod -Method Get -Uri $uri -Headers $herokuRequestHeader -Verbose -Debug
+Write-Output "GET Request URL: $uri"
+$reviewAppInstance = $appReviewList.where{$_.branch.trim() -eq $branchName}
+if($reviewAppInstance.count -gt 0){
+    Write-Warning "The pipeline: $targetHerokuPipelineName already have a Review APP for the pull request: $pullRequestNumber."
+    Write-Output $reviewAppInstance
+    Write-Output "Finishing execution of the primary review app process...."
+    Write-Output " DONE "
+    # set a control variable to skip the execution of the subsequent tasks
+    #echo "##vso[task.setvariable variable=ReviewAppExists;isOutput=true]Yes" #set variable
+    return
+}
+Write-Output "Review APP Not Found."
+Write-Output "*************************************************"
+
+# github request header definition
+$githubRequestHeader = @{
+    Accept = "application/vnd.github+json"
+    Authorization = "token $githubAccessToken"
+}
+
+# download the source code related with the pull request from github.
+Write-Output "Downloading the source code related with the pull request from github..."
+$uri = "https://api.github.com/repos/$githubRepositoryFullName/tarball/$githubFullBranchName"
+Write-Output "GET Request URL: $uri"
+$sourceCodeFileName = $githubRepositoryFullName.Replace('/', '___') + "-pr-$pullRequestNumber.tgz"
+$sourceCodeDownloadPath = "$workingDirectoryPath/$sourceCodeFileName"
+try{
+    Invoke-RestMethod -Uri $uri -Headers $githubRequestHeader -Method Get -OutFile $sourceCodeDownloadPath -Verbose -Debug | Out-Null
+    Write-Output "Source Code Found."
+    Write-Output "Downloading Path: $sourceCodeDownloadPath"
+
+    # set output variables
+    #echo "##vso[task.setvariable variable=ReviewAppSourceCodeDownloadPath;isOutput=true]$sourceCodeDownloadPath"
+    #echo "##vso[task.setvariable variable=ReviewAppSourceCodeFileName;isOutput=true]$sourceCodeFileName"
+}
+catch{
+    $exceptionMessage = $_.Exception.Message
+    if($exceptionMessage -imatch "(404) Not Found"){
+        throw("Source Code Not Found. Please verify that all values in the URL are correct.")
+    }
+    else{
+        throw("Unexpected Error. $exceptionMessage.")
+    }
+}
+Write-Output "*************************************************"
 
 
 
