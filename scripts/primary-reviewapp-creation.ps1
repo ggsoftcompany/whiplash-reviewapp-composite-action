d696102e14c4b977568f726981d091611b91ec72
Julio Garcia <juliocgm83@gmail.com>
2022-11-10 13:57:57 -0500

    try 3

--- scripts/primary-reviewapp-creation.ps1
+++ scripts/primary-reviewapp-creation.ps1
@@ -1,9 +1,45 @@
  param (
     [Parameter(Mandatory=$false)]
-    [string]$workingDirectoryPath = "lola"
+    [string]$workingDirectoryPath = "lola",
+
+    [Parameter(Mandatory=$true)]
+    [string]$herokuApiKey
 )
 
-write-output "Working directory: $workingDirectoryPath"
-#write-output "github-pat: ${{ inputs.github-pat }}"
+
+# variables
+$herokuApiBaseURL = "https://api.heroku.com"
+
+# heroku request header definition
+$herokuRequestHeader = @{
+    "Authorization" = "Bearer $herokuApiKey"
+    "Content-Type" = "application/json"
+    "Accept" = "application/vnd.heroku+json;version=3"
+}
+
+# verify if heroku pipeline exists
+$herokuPipelineInstance = $null
+$uri = "$herokuApiBaseURL/pipelines/$targetHerokuPipelineName"
+try{
+    Write-Output "Getting primary HEROKU pipeline details..."
+    Write-Output "GET Request URL: $uri"
+    $herokuPipelineInstance = Invoke-RestMethod -Method Get -Uri $uri -Headers $herokuRequestHeader -Verbose -Debug
+    Write-Output $herokuPipelineInstance
+}
+catch{
+    $exceptionMessage = $_.Exception.Message
+    Write-Output $exceptionMessage
+    Write-Output $_.ErrorDetails
+    if($exceptionMessage -imatch "Not Found"){
+        throw("Primary HEROKU Pipeline Not Found. Please verify that the provided pipeline name is correct.")
+    }
+    else{
+        throw("Unexpected Error. $exceptionMessage.")
+    }
+}
+Write-Output "*************************************************"
+
+
+
 
 write-output "== DONE =="
